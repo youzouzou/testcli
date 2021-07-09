ea4343d8d01ffba467293bea73587de3029de2b6
youguifang <guifang.you@tcl.com>
2021-07-09 23:31:24 +0800

    complete demo

--- test.js
+++ test.js
@@ -1,11 +1,50 @@
 #! /usr/bin/env node
-console.log('hello test-cli')
-
-const download = require('download-git-repo');
+const figlet = require('figlet');
+figlet('My CLI!', {horizontalLayout:"full"}, function(err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    console.log(data)
+    ask()
+});
 
 const inquirer = require("inquirer");
+const Git = require("nodegit");
+const process = require('child_process');
 
-inquirer
+/** 克隆远程仓库代码 */
+const gitClone = (url, path, cb)=>{
+    console.log("正在下载远程仓库代码...")
+    Git.Clone(url, path)
+       .then(function(res) {
+            console.log("下载完成")
+            cb(true)
+        })
+        .catch(function(err) { console.log("下载失败"+err);cb(false) });
+}
+
+/** 下载 */
+const install = (path)=>{
+    console.log("正在安装依赖包...")
+    const cmd = 'cd '+path+' && yarn';
+    process.exec(cmd, function(error, stdout, stderr) {
+        if(error){
+            console.log(error);
+        }
+        if(stdout){
+            console.log(stdout);
+        }
+        if(stderr){
+            console.log(stderr);
+        }
+        console.log("安装完成")
+    });
+}
+
+const ask = ()=>{
+    inquirer
     .prompt([
         {
             type: "input",
@@ -15,20 +54,26 @@ inquirer
         {
             type: "list",
             name: "source",
-            message: "请选择源码",
-            choices: ["vue-element-admin", "api-easy-mock"],
+            message: "请选择模板",
+            choices: ["vue", "react"],
         }
     ])
     .then((res) => {
         console.log(res);
         const { project, source } = res;
-        if (source === "vue-element-admin") {
-            download('direct:https://gitee.com/panjiachen/vue-element-admin.git', project, { clone: true }, function (err) {
-                console.log(err ? 'vue-element-admin下载失败' : 'vue-element-admin下载成功')
+        if (source === "vue") {
+            gitClone("https://github.com/vuejs/vue.git", project, (isSuccess)=>{
+                if(isSuccess){
+                    install(project)
+                }
             })
-        } else if (source === "api-easy-mock") {
-            download('github:https://github.com:youzouzou/api-easy-mock#main', project, { clone: true }, function (err) {
-                console.log(err ? 'api-easy-mock下载失败' : 'api-easy-mock下载成功')
+        } else if (source === "react") {
+            gitClone("https://github.com/facebook/react.git", project, (isSuccess)=>{
+                if(isSuccess){
+                    install(project)
+                }
             })
         }
     });
+}
+
